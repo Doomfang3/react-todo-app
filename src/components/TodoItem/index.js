be27523aa63682c78c785d408e36aaae4931210b
Mathieu Saubin <saubinm@free.fr>
2019-04-05 09:17:48 +0200

    add label for the TodoItem input

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -4,15 +4,17 @@ import "./index.css";
 function TodoItem(props) {
   return (
     <div className="todo-item">
-      <input
-        type="checkbox"
-        data-id={props.item.id}
-        checked={props.item.completed}
-        onChange={props.handleChange}
-      />
-      <p className={props.item.completed ? "completed" : null}>
-        {props.item.text}
-      </p>
+      <label>
+        <input
+          type="checkbox"
+          data-id={props.item.id}
+          checked={props.item.completed}
+          onChange={props.handleChange}
+        />
+        <span className={props.item.completed ? "completed" : null}>
+          {props.item.text}
+        </span>
+      </label>
     </div>
   );
 }
